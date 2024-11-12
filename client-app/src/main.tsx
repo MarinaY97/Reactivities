29d0fe9dc8651e91c145b293d158060b899bcfb5
MarinaY97 <marinaaay1997@gmail.com>
2024-11-12 17:58:42 +0200

    End of section 11

--- client-app/src/main.tsx
+++ client-app/src/main.tsx
@@ -1,5 +1,6 @@
 import 'react-calendar/dist/Calendar.css'
 import 'react-toastify/dist/ReactToastify.min.css'
+import 'react-datepicker/dist/react-datepicker.css'
 import { createRoot } from 'react-dom/client'
 import 'semantic-ui-css/semantic.min.css'
 import './app/layout/styles.css'
